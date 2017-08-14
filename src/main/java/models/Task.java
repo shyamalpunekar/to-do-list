5f50e102ffa8fce52e2dc3ca041c4c6e490e964d
Shyamal Punekar and Stephanie Mayer <stephanie@>
2017-08-14 11:46:52 -0700

    add TaskTest

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -1,59 +1,64 @@
+
 package models;
 
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
 
 public class Task {
-    private int id;
+
     private String description;
+    private static ArrayList<Task> instances = new ArrayList<>();
     private boolean completed;
     private LocalDateTime createdAt;
 
-    public Task(String description) {
-        this.id = id;
+
+    private int id;
+
+    public Task(String description){
         this.description = description;
+        this.completed = false;
         this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static ArrayList<Task> getAll(){
+        return instances;
+    }
+
+    public static void clearAllTasks(){
+        instances.clear();
+    }
+
+    public boolean getCompleted(){
+        return this.completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 
     public int getId() {
         return id;
     }
 
+    public static Task findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    }
+
+    public void update(String content) {
+        this.description = content;
+    }
+
+    public void deleteTask(){
+        instances.remove(id-1); //same reason
+    }
     public void setId(int id) {
         this.id = id;
     }
 
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Task task = (Task) o;
-
-        if (completed != task.completed) return false;
-        if (id != task.id) return false;
-        return description != null ? description.equals(task.description) : task.description == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = description != null ? description.hashCode() : 0;
-        result = 31 * result + (completed ? 1 : 0);
-        result = 31 * result + id;
-        return result;
-    }
-
-
-
-
 }
-
